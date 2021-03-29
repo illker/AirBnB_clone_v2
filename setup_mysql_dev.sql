fb5f5500b610503fac3834ce85e9b03cf6d382f7
eduard moreno <katananata500@gmail.com>
2021-03-29 07:25:43 +0000

    fixed delete space

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---Script that prepare a MySQL server for the project
+-- Script that prepare a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
