a0bb28b68fa9abcdbf06141b007f189eaa205888
eduard moreno <katananata500@gmail.com>
2021-03-29 09:53:23 +0000

    change string for String

--- models/user.py
+++ models/user.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, string
+from os import getenv
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
-    email = Column(string(128), nullable=False)
-    password = Column(string(128), nullable=False)
-    first_name = Column(string(128), nullable=True)
-    last_name = Column(string(128), nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
