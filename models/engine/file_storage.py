0b5187ab5aa1da852a03aec02b0578540f01387c
David Alzate <illker@hotmail.com>
2021-03-30 20:50:52 +0000

    do: def felete + all :notes:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            class_obj = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__:
+                    class_obj[key] = value
+            return class_obj
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
