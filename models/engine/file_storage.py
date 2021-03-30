21844339ff30aab4e861f5fab2ac0468288a4603
David Alzate <illker@hotmail.com>
2021-03-30 17:04:12 +0000

    adding delete and close :innocent:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            container = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    container[k] = v
-            return container
-        else:
-            return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -49,25 +36,26 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
-def delete(self, obj=None):
-    """To delete obj from __objects if its inside - if obj is equal
-    to None, the method should not do anything"""
-
-    
-     def delete(self, obj=None):
-        """Delete a object inside the dict objects."""
+    def delete(self, obj=None):
+        """delete obj from __objects if its inside"""
         if obj is not None:
-            del self.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)]
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
