4bba9d3415fc05d1598a5db83d3502089548e693
eduard moreno <katananata500@gmail.com>
2021-03-31 23:13:11 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
-from os import getenv as env
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import backref, relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    if env("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        kwargs = {"cascade": "all, delete-orphan", "backref": "user"}
-        places = relationship("Place", **kwargs)
-        reviews = relationship("Review", **kwargs)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
