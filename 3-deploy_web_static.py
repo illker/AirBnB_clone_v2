d2c7153b5cefd89b7bdc09e3cc9eeb489b7af2a2
illker <illker@hotmail.com>
2021-04-15 22:11:08 -0500

    Full deployment :trollface:

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Full deployment"""
+
+from fabric.api import *
+from os import path
+
+env.hosts = ['35.237.105.160', '54.221.65.216']
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    if isdir("versions") is False:
+        local("mkdir -p versions")
+    d = datetime.now().strftime("%Y%m%d%H%M%S")
+    burger = "versions/web_static_{}.tgz".format(d)
+    if local("tar -czvf {} web_static/".format(burger)):
+        return burger
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """Fabric script distributes an archive to your web servers"""
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, "/tmp/")
+        burger = archive_path.split('/')[-1]
+        filedir = burger.split('.')[0]
+        pathf = "/data/web_static/releases/" + filedir
+        run("mkdir -p " + pathf)
+        run("tar -xzf /tmp/" + burger + " -C " + pathf)
+        run("rm /tmp/" + burger)
+        run("mv " + pathf + "/web_static/* " + pathf)
+        run("rm -rf " + pathf + "/web_static/")
+        run("rm -rf /data/web_static/current")
+        run("ln -sf " + pathf + "/" + " /data/web_static/current")
+
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """Fabric script that creates and distributes an archive to your web servers"""
+
+    burger = do_pack()
+    if not path.exists(burger):
+        return False
+    archive_path = "versions/" + burger
+    return do_deploy(archive_path)
