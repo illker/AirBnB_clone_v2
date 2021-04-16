e7eb21645fdffefa72210b5e1dc334cf3e93291e
illker <illker@hotmail.com>
2021-04-15 22:41:04 -0500

    Full deployment :trollface:

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 """Full deployment"""
 
-from fabric.api import *
-from os import path
-from os.path import isdir
+from fabric.api import env, local, put, run
 from datetime import datetime
+from os.path import exists, isdir
 
 env.hosts = ['35.237.105.160', '54.221.65.216']
 
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
-    if isdir("versions") is False:
-        local("mkdir -p versions")
-    d = datetime.now().strftime("%Y%m%d%H%M%S")
-    burger = "versions/web_static_{}.tgz".format(d)
-    if local("tar -czvf {} web_static/".format(burger)):
-        return burger
-    else:
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file))
+        return file
+    except:
         return None
 
 
 def do_deploy(archive_path):
     """Fabric script distributes an archive to your web servers"""
-    if not path.exists(archive_path):
-        return False
-
+    if exists(archive_path) is False:
+        return (False)
     try:
-        put(archive_path, "/tmp/")
-        burger = archive_path.split('/')[-1]
-        filedir = burger.split('.')[0]
-        pathf = "/data/web_static/releases/" + filedir
-        run("mkdir -p " + pathf)
-        run("tar -xzf /tmp/" + burger + " -C " + pathf)
-        run("rm /tmp/" + burger)
-        run("mv " + pathf + "/web_static/* " + pathf)
-        run("rm -rf " + pathf + "/web_static/")
-        run("rm -rf /data/web_static/current")
-        run("ln -sf " + pathf + "/" + " /data/web_static/current")
-
-        return True
+        file = archive_path.split("/")[-1]
+        x = file.split(".")[0]
+        route = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(route, x))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file, route, x))
+        run('rm /tmp/{}'.format(file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(route, x))
+        run('rm -rf {}{}/web_static'.format(route, x))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(route, x))
+        return (True)
     except:
-        return False
+        return (False)
 
 
 def deploy():
